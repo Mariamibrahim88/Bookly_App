92503949a9e545ec56215448d1641ce69c88e4c0
maria <mariamibrahimmi4142726@gmail.com>
2024-03-19 23:23:51 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color KprimaryColor = Color(0xff100B20);
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SvgPicture.asset(
+            AssetsData.logo,
+            width: 200,
+            height: 50,
+          ),
+        ]);
+  }
+}
