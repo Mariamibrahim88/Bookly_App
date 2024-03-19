133ae032765a16507b1459a6a1f398eabb68fc15
maria <mariamibrahimmi4142726@gmail.com>
2024-03-20 01:18:49 +0200

    adding animation to splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,11 +1,43 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  @override
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -16,6 +48,8 @@ class SplashViewBody extends StatelessWidget {
             width: 250,
             height: 60,
           ),
+          const SizedBox(height: 10),
+          SlidingText(slidingAnimation: slidingAnimation),
         ]);
   }
 }
