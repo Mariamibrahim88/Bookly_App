283acfd55bd8d6f1a05a7078b4d8e4334cd7b308
maria <mariamibrahimmi4142726@gmail.com>
2024-05-02 14:28:53 +0300

    fetch similar books done

--- lib/features/home/presentation/views/widgets/best_seller_book_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_book_item.dart
@@ -16,7 +16,7 @@ class BestSellerBookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        GoRouter.of(context).push('/bookDetailsView');
+        GoRouter.of(context).push('/bookDetailsView', extra: bookModel);
       },
       child: SizedBox(
         height: 115,
--- lib/features/home/presentation/views/widgets/similar_book_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_book_list_view.dart
@@ -1,28 +1,44 @@
+import 'package:bookly_app/core/widgets/custom_erorr_message.dart';
+import 'package:bookly_app/core/widgets/custom_loading.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured__list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBookListView extends StatelessWidget {
   const SimilarBookListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .134,
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        //itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: FeatureListViewItem(
-              imageurl:
-                  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gettyimages.com%2Fphotos%2Fbooks&psig=AOvVaw1jucGB_ggkpTeXGjkmEfCC&ust=1713910379328000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDF9evr1oUDFQAAAAAdAAAAABAE',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .134,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              //itemCount: 10,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: FeatureListViewItem(
+                    imageurl:
+                        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gettyimages.com%2Fphotos%2Fbooks&psig=AOvVaw1jucGB_ggkpTeXGjkmEfCC&ust=1713910379328000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDF9evr1oUDFQAAAAAdAAAAABAE',
+                  ),
+                );
+              },
             ),
+            //child: CustomListView())],
           );
-        },
-        //child: CustomListView())],
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErorrMessage(
+            errorMessage: state.errormessage,
+          );
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
