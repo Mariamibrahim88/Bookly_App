a546ecd9906d2a53ec1a36b562053ac7dd1faad8
maria <mariamibrahimmi4142726@gmail.com>
2024-05-16 21:46:01 +0300

    create search repo

---
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl extends SearchRepo {
+  final ApiService apiService;
+
+  SearchRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchedBooks(
+      {required String searchedBook}) async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:$searchedBook');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+      // TODO
+    }
+  }
+}
